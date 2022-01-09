113869ce288d587181743fa96321c2548c49def9
Jason Edokpa <jasonedokpa@gmail.com>
2022-01-09 10:53:41 -0600

    cors fix

--- backend/my_games_list_proj/settings.py
+++ backend/my_games_list_proj/settings.py
@@ -26,10 +26,12 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     "my_games_list_app",
+    "corsheaders",
     "rest_framework"
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -113,3 +115,12 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ALLOWED_ORIGINS = [
+    "https://example.com",
+    "https://sub.example.com",
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+    "http://localhost:8080",
+    "http://localhost:8000",
+]
--- frontend/my-games-list/src/pages/ListofLists.js
+++ frontend/my-games-list/src/pages/ListofLists.js
@@ -6,28 +6,25 @@ import importObject from "../api/my-games-list-api"
 function ListsofLists(props)
 {
 	// states
-	const [gamesLists, setGamesLists] = useState(0)
+	const [gamesLists, setGamesLists] = useState([0,1])
 
 	// effects
 	// useEffect("function")
 	useEffect(() => {
 		const getGamesList = async () => 
 		{
-			const data = importObject.fetchAllGamesLists()
-			console.log("returned data", data)
+			const data = await importObject.fetchAllGamesLists()
 			if (data)
-			{
 				setGamesLists(data)
-			}
 		}
 
 		getGamesList()
 	}, [])
 
-	const renderGamesList = () => {
-		let elems = gamesLists.map((gamesList) => {
+	const renderGamesLists = () => {
+		let elems = gamesLists.map((gamesList, index) => {
 			return	(
-				<li>
+				<li key={index}>
 					<Link to ={`/games-list/${gamesList.id}`}>{gamesList.name}</Link>
 				</li>
 			) 
@@ -37,10 +34,8 @@ function ListsofLists(props)
 
 	return	( 
 		<div className = "ListofLists">
-			<p>HelloWorld</p>
-			<ul>
-				{renderGamesList()}
-			</ul>
+			<h2>Choose A List</h2>
+			{renderGamesLists()}
 		</div>
 	)
 }
